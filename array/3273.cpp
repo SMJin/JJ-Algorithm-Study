c753fd9e096fef869c4171e57f03702f2c90ceb3
SMJin <sstaw@naver.com>
2021-07-22 20:01:54 +0900

    edit 3273.cpp

--- array/3273.cpp
+++ array/3273.cpp
@@ -8,12 +8,13 @@ int main() {
 	int* arr = new int[n];
 	for (int i = 0; i < n; i++) cin >> arr[i];
 	cin >> x;
+	int* arr2 = new int[x + 1]{};
+	for (int i = 0; i < n; i++) if (arr[i] <= x) arr2[arr[i]]++;
 
 	int result = 0;
-	for (int i = 0; i < n; i++) for (int j = i + 1; j < n; j++) if (arr[i] + arr[j] == x) result++;
+	for (int i = 1; i < (x+1) / 2; i++) if (arr2[i] && arr2[x - i]) result++;
 
-	cout << result;
-	delete[] arr;
-	
+	if (arr2[0] && arr2[x]) result++;
+	cout << result;	
 	return 0;
 }
